cea434ffc7b9b78661421aca4da8e0032b7a466a
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2012-09-08 00:38:36 -0500

    refactoring the specs for Util with some minor code polishing

--- lib/tableficate/utils.rb
+++ lib/tableficate/utils.rb
@@ -3,7 +3,10 @@ module Tableficate
     def self.template_path(template, partial_name, theme = '')
       file_path = File.join(['tableficate', theme, partial_name].delete_if(&:empty?))
 
-      file_path = File.join(['tableficate', partial_name]) if !theme.empty? && !template.lookup_context.exists?(file_path, [], true)
+      # We have a theme but no matching partial so we fall back to the default partial.
+      if !theme.empty? && !template.lookup_context.exists?(file_path, [], true)
+        file_path = File.join(['tableficate', partial_name])
+      end
 
       file_path
     end
--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -4,27 +4,35 @@ describe Tableficate::Utils do
   describe ".template_path(template, partial_name, theme = '')" do
     before(:each) do
       @template = mock('Template')
-      @template.stub!(:lookup_context).and_return(ActionView::LookupContext.new([]))
     end
 
-    it 'should construct a path with no theme' do
-      @template.lookup_context.stub(:exists?).and_return(true)
+    context 'theme' do
+      context 'is blank' do
+        it 'returns a path' do
+          @template.stub_chain(:lookup_context, :exists?).and_return(true)
 
-      Tableficate::Utils::template_path(@template, 'table').should == 'tableficate/table'
-    end
-
-    it 'should construct a path with a theme' do
-      @template.lookup_context.stub(:exists?).and_return(true)
-
-      Tableficate::Utils::template_path(@template, 'table', 'futuristic').should == 'tableficate/futuristic/table'
-    end
-
-    it 'should fallback to a path with no theme if the theme does not have the requested partial' do
-      @template.lookup_context.should_receive(:exists?) do |*args|
-        (args.first == 'tableficate/table')
+          Tableficate::Utils::template_path(@template, 'table').should == 'tableficate/table'
+        end
       end
 
-      Tableficate::Utils::template_path(@template, 'table', 'futuristic').should == 'tableficate/table'
+      context 'is not blank' do
+        it 'returns a path with the theme' do
+          @template.stub_chain(:lookup_context, :exists?).and_return(true)
+
+          Tableficate::Utils::template_path(@template, 'table', 'futuristic').should == 'tableficate/futuristic/table'
+        end
+
+        context 'has no matching partial' do
+          it 'falls back to a path with no theme' do
+            @template.stub!(:lookup_context).and_return(ActionView::LookupContext.new([]))
+            @template.lookup_context.should_receive(:exists?) do |*args|
+              (args.first == 'tableficate/table')
+            end
+
+            Tableficate::Utils::template_path(@template, 'table', 'futuristic').should == 'tableficate/table'
+          end
+        end
+      end
     end
   end
 end
