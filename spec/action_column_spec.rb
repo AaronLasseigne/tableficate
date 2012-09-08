f13b052cdcfd1d5f7db618e215cc6b097a31b184
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2012-09-07 21:38:27 -0500

    reworking action column tests

--- spec/action_column_spec.rb
+++ spec/action_column_spec.rb
@@ -1,29 +1,9 @@
 require 'spec_helper'
 
 describe Tableficate::ActionColumn do
-  before(:all) do
-    @action_column = Tableficate::ActionColumn.new(nil) do
-      'Actions Here!'
-    end
-  end
+  subject { described_class.new(double('Table')) }
 
-  it 'should always have sorting off ' do
-    @action_column.show_sort?.should be false
-  end
-
-  it 'should always indicate that it is not sorted' do
-    @action_column.is_sorted?.should be false
-  end
-
-  it 'should have a blank name' do
-    @action_column.name.should == ''
-  end
-
-  it 'should accept options' do
-    action_column = Tableficate::ActionColumn.new(nil, header: 'Actions') do
-      'Actions Here!'
-    end
-
-    action_column.header.should == 'Actions'
-  end
+  its(:show_sort?) { should be_false }
+  its(:is_sorted?) { should be_false }
+  its(:name)       { should == '' }
 end
