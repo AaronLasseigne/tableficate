ff4815265d329bb19f7f418652f2630e8d7428f7
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2012-12-27 21:08:18 -0600

    adding rspec configuration to the spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,10 @@ require File.expand_path('../test_app/config/environment', __FILE__)
 require 'rspec/rails'
 require 'capybara/rspec'
 require 'capybara/rails'
+
+Rspec.configure do |config|
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run_including :focus
+  config.filter_run_excluding :ignore
+end
