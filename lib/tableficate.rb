d0248d448c8235c95699ed3d7188fd0bdc8b85b2
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2013-11-19 21:09:01 -0600

    clearing out requires that are no longer around

--- lib/tableficate.rb
+++ lib/tableficate.rb
@@ -1,16 +1,11 @@
 require 'tableficate/engine'
-require 'tableficate/exceptions'
 require 'tableficate/utils'
-require 'tableficate/finder'
 require 'tableficate/column'
 require 'tableficate/action_column'
 require 'tableficate/caption'
 require 'tableficate/empty'
 require 'tableficate/table'
 require 'tableficate/helper'
-require 'tableficate/base'
-require 'tableficate/active_record_extension'
 require 'tableficate/version'
 
 ActionView::Base.send(:include, Tableficate::Helper)
-ActiveRecord::Base.send(:include, Tableficate::ActiveRecordExtension)
