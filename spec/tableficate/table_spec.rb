1343cf78eab9bde958498e0518d98ddedea5300f
Aaron Lasseigne <aaron.lasseigne@gmail.com>
2014-01-06 22:11:15 -0600

    removing leftover shared example

--- spec/tableficate/table_spec.rb
+++ spec/tableficate/table_spec.rb
@@ -1,31 +1,5 @@
 require 'spec_helper'
 
-shared_examples 'a getter and setter returning an object' do |method, klass|
-  context 'has args' do
-    it 'acts as a setter' do
-      subject.send(method, 'Foo')
-
-      expect(subject.send(method)).to be_instance_of(klass)
-    end
-  end
-
-  context 'has a block' do
-    it 'acts as a setter' do
-      subject.send(method, Proc.new { 'Foo' })
-
-      expect(subject.send(method)).to be_instance_of(klass)
-    end
-  end
-
-  context 'has no args and no block' do
-    it 'acts as a getter' do
-      subject.send(method, 'Foo')
-
-      expect(subject.send(method)).to be_instance_of(klass)
-    end
-  end
-end
-
 describe Tableficate::Table do
   include_context 'view context'
 
